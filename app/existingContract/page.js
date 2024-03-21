55dd8c0d79e30c8493b775fa219d90087cc2c41f
Johan De Coster <johan.decoster@etexgroup.com>
2024-03-21 13:45:37 +0100

    update backend

--- app/existingContract/page.js
+++ app/existingContract/page.js
@@ -131,7 +131,7 @@ const MyPage = () => {
 	
 
   async function updateContractInfoSeller(senderwallet, receiverwallet, APN){
-    const apiUrl = 'https://smartcrow-backend-goerli-2.onrender.com/api/update-contract'; 
+    const apiUrl = 'https://smartcrow-polygon-mainnet.onrender.com/api/update-contract'; 
     console.log('sender: '+senderwallet);
     console.log('receiver: '+receiverwallet);
     console.log('APN: '+APN);
@@ -175,7 +175,7 @@ const MyPage = () => {
   }
   
   async function updateContractInfoReceiver(senderwallet, receiverwallet, APN){
-    const apiUrl = 'https://smartcrow-backend-goerli-2.onrender.com/api/update-contract'; 
+    const apiUrl = 'https://smartcrow-polygon-mainnet.onrender.com/api/update-contract'; 
   
     // Example data to send in the request body
     const requestData = {
